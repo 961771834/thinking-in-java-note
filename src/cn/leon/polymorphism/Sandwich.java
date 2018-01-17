6bcb6c7301959459b467f89a5ce35b5694c18378
961771834 <961771834@qq.com>
2018-01-18 07:50:30 +0800

    update

---
+++ src/cn/leon/polymorphism/Sandwich.java
@@ -0,0 +1,46 @@
+package cn.leon.polymorphism;
+//: polymorphism/cn.leon.polymorphism.Sandwich.java
+// Order of constructor calls.
+
+
+class Meal {
+    Meal() { System.out.println("Meal()"); }
+}
+
+class Bread {
+    Bread() { System.out.println("Bread()"); }
+}
+
+class Cheese {
+    Cheese() { System.out.println("Cheese()"); }
+}
+
+class Lettuce {
+    Lettuce() { System.out.println("Lettuce()"); }
+}
+
+class Lunch extends Meal {
+    Lunch() { System.out.println("Lunch()"); }
+}
+
+class PortableLunch extends Lunch {
+    PortableLunch() { System.out.println("PortableLunch()");}
+}
+
+public class Sandwich extends PortableLunch {
+    private Bread b = new Bread();
+    private Cheese c = new Cheese();
+    private Lettuce l = new Lettuce();
+    public Sandwich() { System.out.println("cn.leon.polymorphism.Sandwich()"); }
+    public static void main(String[] args) {
+        new Sandwich();
+    }
+} /* Output:
+Meal()
+Lunch()
+PortableLunch()
+Bread()
+Cheese()
+Lettuce()
+cn.leon.polymorphism.Sandwich()
+*///:~
